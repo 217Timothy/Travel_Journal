947284232d723e7e97efd53d132efaea5ee145bd
張正璽 <timchang0217@gmail.com>
2026-05-23 16:11:26 +0800

    initialize

--- backend/src/router/api/v1/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from src.router.api.v1.endpoints import auth, trips
-
-api_router = APIRouter()
-
-api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
-api_router.include_router(trips.router, prefix="/trips", tags=["Trips"])
